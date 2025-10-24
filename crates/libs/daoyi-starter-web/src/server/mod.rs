15757458e20d286d13d759c7b623799cd8d9c213
Gemiman <913573586@qq.com>
2025-10-25 00:23:48 +0800

    feat(web): 集成 tracing 并优化服务启动日志- 添加 tracing依赖以支持结构化日志记录 - 在服务启动时打印当前日志级别配置 - 打印 OpenAPI 页面访问地址（Scalar 和 SwaggerUI）-优化 TCP 监听器绑定逻辑并增强可读性

--- crates/libs/daoyi-starter-web/src/server/mod.rs
+++ crates/libs/daoyi-starter-web/src/server/mod.rs
@@ -1,19 +1,23 @@
 use daoyi_common::common::conf;
-use salvo::conn::TcpListener;
+use salvo::conn::{Acceptor, TcpListener};
 use salvo::oapi::OpenApi;
 use salvo::prelude::{Scalar, SwaggerUi};
 use salvo::{Listener, Router, Server};
 
 pub async fn start(router: Router) -> anyhow::Result<()> {
     let c = conf::get().await;
+    tracing::info!("log level: {}", c.server().log_level());
     let doc = OpenApi::new("salvo web api", "0.0.1").merge_router(&router);
     let router = router
         .unshift(doc.into_router("/api-doc/openapi.json"))
         .unshift(Scalar::new("/api-doc/openapi.json").into_router("scalar"))
         .unshift(SwaggerUi::new("/api-doc/openapi.json").into_router("swagger"));
-    let acceptor = TcpListener::new(format!("0.0.0.0:{}", c.server().port()))
-        .bind()
-        .await;
+    let local_addr = format!("0.0.0.0:{}", c.server().port());
+    let acceptor = TcpListener::new(local_addr).bind().await;
+    acceptor.holdings().iter().for_each(|h| {
+        tracing::info!("📖 Open API Page With Scaler: https://{h}/scalar");
+        tracing::info!("📖 Open API Page With SwaggerUi://{h}/swagger");
+    });
     Server::new(acceptor).serve(router).await;
     Ok(())
 }
